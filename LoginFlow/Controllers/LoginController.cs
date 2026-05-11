d3dedf7793a621d7322db15d92c682408814887b
gautamsagarnsit <gautamsagarnsit@gmail.com>
2026-05-11 23:14:28 +0530

    Setup Postgres Data Provider and Created Controlleras for Login and Register.

---
+++ LoginFlow/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginFlow.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Login([FromForm] User request)
+        {
+
+            if (request.Username == "admin" && request.Password == "password")
+            {
+                return Ok($"Login Successful: {request.Username}, {request.Password}, {request.Email}");
+            }
+            return Ok($"Login Failed: {request.Username}, {request.Password}");
+        }
+    }
+}
---
+++ LoginFlow/Controllers/RegisterController.cs
@@ -0,0 +1,30 @@
+﻿using LoginFlow.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginFlow.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        public RegisterController(ApplicationDbContext context)
+        {
+            _context  = context;
+        }
+        [HttpPost]
+        public IActionResult Register([FromForm] User request)
+        {
+            _context.Users.Add(request);
+            _context.SaveChanges();
+            return Ok(request);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.Users.ToList());
+        }
+    }
+}
