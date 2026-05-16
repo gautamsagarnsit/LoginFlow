965a681cba959d01ebd054811f8ce2c0baaf6046
gautamsagarnsit <gautamsagarnsit@gmail.com>
2026-05-16 20:50:59 +0530

    Created Remote DB Instance and connection setup.

---
+++ LoginFlow/Controllers/AccessDenied.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoginFlow.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccessDenied : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Your are not allowed to access the content, signIn to Access");
+        }
+    }
+}
