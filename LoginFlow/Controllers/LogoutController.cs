feab4d44b05abffd69b56c51f44fd3f531edf485
Gautam Sagar <gautamsagarnsit@gmail.com>
2026-06-05 19:11:18 +0530

    Added Logout

---
+++ LoginFlow/Controllers/LogoutController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginFlow.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogoutController : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public LogoutController(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Ok("User Logged Out");
+        }
+    }
+}
